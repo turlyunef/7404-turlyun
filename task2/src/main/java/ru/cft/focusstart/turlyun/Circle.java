0b23148c531ad35ef389a6743e63c8829f226373
turlyunef <turlyunef@mail.ru>
2019-04-18 11:27:09 +0700

    Circle fixed.

--- task2/src/main/java/ru/cft/focusstart/turlyun/Circle.java
+++ task2/src/main/java/ru/cft/focusstart/turlyun/Circle.java
@@ -2,10 +2,10 @@ package ru.cft.focusstart.turlyun;
 
 import java.util.LinkedHashMap;
 
-public class Circle extends SomeShape  {
+public class Circle extends SomeShape {
     private int radius;
 
-    public Circle(int radius){
+    public Circle(int radius) {
         this.radius = radius;
     }
 
@@ -22,38 +22,37 @@ public class Circle extends SomeShape  {
     public LinkedHashMap<String, String> getShapeName() {
         LinkedHashMap<String, String> circleName = new LinkedHashMap<>();
         circleName.put("Название", "Круг");
+
         return circleName;
     }
 
     @Override
     public LinkedHashMap<String, String> getShapeArea() {
         LinkedHashMap<String, String> circleArea = new LinkedHashMap<>();
-        circleArea.put("Название", "Круг");
+        circleArea.put("Площадь", String.format("%.2f", Math.PI * radius * radius));
+
         return circleArea;
     }
 
     @Override
     public LinkedHashMap<String, String> getPerimeterOfTheFigure() {
         LinkedHashMap<String, String> perimeterOfTheCircle = new LinkedHashMap<>();
-        perimeterOfTheCircle.put("Периметр", String.valueOf(2 * Math.PI * radius));
+        perimeterOfTheCircle.put("Периметр", String.format("%.2f", 2 * Math.PI * radius));
+
         return perimeterOfTheCircle;
     }
 
-    public LinkedHashMap<String, String> getRadius(){
+    public LinkedHashMap<String, String> getRadius() {
         LinkedHashMap<String, String> circleRadius = new LinkedHashMap<>();
         circleRadius.put("Радиус", String.valueOf(radius));
 
         return circleRadius;
     }
 
-    public LinkedHashMap<String, String> getDiameter(){
+    public LinkedHashMap<String, String> getDiameter() {
         LinkedHashMap<String, String> circleDiameter = new LinkedHashMap<>();
         circleDiameter.put("Диаметр", String.valueOf(2 * radius));
 
         return circleDiameter;
-    }
-
-    public void setRadius(int radius) {
-        this.radius = radius;
     }
 }
